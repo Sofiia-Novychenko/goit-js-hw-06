c66f6204cf677d4ff841681d6ce2c9326263446b
Vyshenko Njanko <milakotiy@gmail.com>
2024-12-08 21:25:45 +0100

    finish all the tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,28 @@
-const filterArray = function (numbers, value) {
-  let array = [];
-  for (let elements of numbers) {
-    if (elements > value) {
-      array.push(elements);
-    }
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
   }
-  return array;
-};
-
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    // this.#value = str + this.#value + str;
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
